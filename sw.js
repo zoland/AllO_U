2300e2be63f199e2d0c02d055629ab454669a59f
Zoland <zolandv@gmail.com>
2025-10-03 14:22:32 +0700

    css 4

--- sw.js
+++ sw.js
@@ -1,58 +1,47 @@
 // sw.js
-const CACHE_NAME = 'allo-u-v1.2.1';
-const urlsToCache = [
-    './',
-    './index.html',
-    './css/styles.css',
-    './js/app.js',
-    './js/models/Event.js',
-    './js/models/Participant.js',
-    './js/models/Profile.js',
-    './js/models/Scene.js',
-    './js/services/ImageService.js',
-    './js/services/StorageService.js',
-    './js/services/TestDataService.js',
-    './js/views/EventsView.js',
-    './js/views/LocatorView.js',
-    './js/views/ScenesView.js',
-    './js/views/TeamView.js',
-    './js/utils/Constants.js',
-    './manifest.json'
+const CACHE_NAME = 'allo-cache-v2';
+const PRECACHE_URLS = [
+  './',
+  './index.html',
+  './css/styles.css',
+  './js/app.js',
+  './js/utils/Constants.js',
+  './js/models/Scene.js',
+  './js/models/Participant.js',
+  './js/models/Event.js',
+  './js/models/Profile.js',
+  './js/services/StorageService.js',
+  './js/services/TestDataService.js',
+  './js/services/ImageService.js',
+  './js/views/ScenesView.js',
+  './js/views/TeamView.js',
+  './js/views/EventsView.js',
+  './js/views/LocatorView.js',
+  './manifest.json'
 ];
 
 self.addEventListener('install', event => {
-    event.waitUntil(
-        caches.open(CACHE_NAME)
-            .then(cache => {
-                console.log('Opened cache');
-                return cache.addAll(urlsToCache);
-            })
-    );
-});
-
-self.addEventListener('fetch', event => {
-    event.respondWith(
-        caches.match(event.request)
-            .then(response => {
-                if (response) {
-                    return response;
-                }
-                return fetch(event.request);
-            })
-    );
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(PRECACHE_URLS))
+  );
 });
 
 self.addEventListener('activate', event => {
-    const cacheWhitelist = [CACHE_NAME];
-    event.waitUntil(
-        caches.keys().then(cacheNames => {
-            return Promise.all(
-                cacheNames.map(cacheName => {
-                    if (cacheWhitelist.indexOf(cacheName) === -1) {
-                        return caches.delete(cacheName);
-                    }
-                })
-            );
-        })
-    );
+  event.waitUntil(
+    caches.keys().then(keys => 
+      Promise.all(keys
+        .filter(key => key !== CACHE_NAME)
+        .map(key => caches.delete(key))
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request, { ignoreSearch: true })
+      .then(cached => cached || fetch(event.request))
+  );
 });
